f6b3e6f2449b8134c800e532073be0f947006fdb
Nhib1910117 <nhib1910117@student.ctu.edu.vn>
2023-02-15 15:30:42 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -10,4 +12,5 @@ app.get("/", (req, res) =>{
     res.json({ message: "Welcome to contact book application." });
 
 });
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
